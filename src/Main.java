44a168356ac0a6ee9683a7e6d4200a9dec3a0cfb
BerryPie1 <156555490+BerryPie1@users.noreply.github.com>
2024-12-23 15:38:35 +0800

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("");
+        System.out.println("功能测试");
+        System.out.println("---------------------------");
+
+        System.out.println("");
+        System.out.println("功能测试");
+        System.out.println("---------------------------");
+    }
+}
